e7e7332fd324b27cf3de54f24df73d4bf7ed1f20
IgnGrillo <igngrillo@gmail.com>
2023-06-20 15:16:09 -0300

    Add PublishOnBombPressedShould, adding PublishOnBombPressedWhenExecute test

---
+++ Assets/Features/Cell/Tests/Editor/PublishOnBombPressedShould.cs
@@ -0,0 +1,36 @@
+﻿using Features.Cell.Scripts.Domain.Actions;
+using Features.Cell.Scripts.Domain.Events;
+using NUnit.Framework;
+
+namespace Features.Cell.Tests.Editor
+{
+    public class PublishOnBombPressedShould
+    {
+        private bool _eventBusSubscriptionFlag;
+
+        [Test]
+        public void PublishOnBombPressedWhenExecute()
+        {
+            var publishOnBombPressed = GivenAPresenter();
+            GivenAClearEventBus();
+            GivenAnOnBombPressedEventBusSubscription();
+            WhenExecute(publishOnBombPressed);
+            ThenEventSubscribedWasCalled();
+        }
+
+        private static PublishOnBombPressed GivenAPresenter() => 
+                new();
+
+        private static void GivenAClearEventBus() =>
+                EventBus.Clear();
+
+        private void GivenAnOnBombPressedEventBusSubscription() =>
+                EventBus.Subscribe<OnBombPressed>(_ => _eventBusSubscriptionFlag = true);
+
+        private static void WhenExecute(IPublishOnBombPressed publishOnBombPressed) =>
+                publishOnBombPressed.Execute();
+
+        private void ThenEventSubscribedWasCalled() =>
+                Assert.IsTrue(_eventBusSubscriptionFlag);
+    }
+}
