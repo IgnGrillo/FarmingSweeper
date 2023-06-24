d5e8b8fd589c73d776ec805748c3ee8529365ea4
IgnGrillo <igngrillo@gmail.com>
2023-06-24 19:46:59 -0300

    Add AnimateBoardAppearanceShould, trying to implement the first test

--- Assets/Features/Gameplay/Scripts/Domain/Actions/AnimateBoardAppearance.cs
+++ Assets/Features/Gameplay/Scripts/Domain/Actions/AnimateBoardAppearance.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using UniRx;
+using System.Linq;
+using Features.Cell.Scripts.Domain;
 
 namespace Features.Gameplay.Scripts.Domain.Actions
 {
     public class AnimateBoardAppearance : IAnimateBoardAppearance
     {
-        public IObservable<Unit> Execute(MineSweeperBoard mineSweeperBoard) => 
-                Observable.ReturnUnit();
+        public IObservable<Unit> Execute(MineSweeperBoard mineSweeperBoard)
+        {
+            mineSweeperBoard = GivenA3X3EmptyBoard();
+            var cells = mineSweeperBoard.Cells;
+            var mineSweeperCells = cells.Cast<MineSweeperCell>().ToList();
+            for (var i = 0; i < mineSweeperCells.Count + 1; i++)
+            {
+                mineSweeperCells[i].OnAnimateAppearance
+                                   .Delay(TimeSpan.FromSeconds(.5f))
+                                   .Concat(mineSweeperCells[i + 1].OnAnimateAppearance);
+            }
+            return mineSweeperCells[0].OnAnimateAppearance;
+        }
+        
+        private static MineSweeperBoard GivenA3X3EmptyBoard()
+        {
+            return new MineSweeperBoard
+            {
+                Cells = new[,]
+                {
+                    { GivenACellWith(), GivenACellWith(), GivenACellWith() },
+                    { GivenACellWith(), GivenACellWith(), GivenACellWith() },
+                    { GivenACellWith(), GivenACellWith(), GivenACellWith() }
+                }
+            };
+        }
+        
+        private static MineSweeperCell GivenACellWith(bool isBomb = false,
+                                                      CellSecondaryStatus secondaryStatus = CellSecondaryStatus.Blank,
+                                                      int bombsNearby = 0) => new(isBomb, secondaryStatus, bombsNearby);
     }
 }
---
+++ Assets/Features/Gameplay/Tests/Editor/AnimateBoardAppearanceShould.cs
@@ -0,0 +1,36 @@
+﻿using Features.Cell.Scripts.Domain;
+using Features.Gameplay.Scripts.Domain;
+using Features.Gameplay.Scripts.Domain.Actions;
+using NUnit.Framework;
+
+namespace Features.Gameplay.Tests.Editor
+{
+    public class AnimateBoardAppearanceShould
+    {
+        [Ignore("Wip")]
+        [Test]
+        public void AnimateBoardAppearanceShouldSimplePasses()
+        {
+            var mineSweeperBoard = GivenA3X3EmptyBoard();
+            var animateBoardAppearance = new AnimateBoardAppearance();
+            animateBoardAppearance.Execute(mineSweeperBoard);
+        }
+
+        private static MineSweeperBoard GivenA3X3EmptyBoard()
+        {
+            return new MineSweeperBoard
+            {
+                Cells = new[,]
+                {
+                    { GivenACellWith(), GivenACellWith(), GivenACellWith() },
+                    { GivenACellWith(), GivenACellWith(), GivenACellWith() },
+                    { GivenACellWith(), GivenACellWith(), GivenACellWith() }
+                }
+            };
+        }
+
+        private static MineSweeperCell GivenACellWith(bool isBomb = false,
+                                                      CellSecondaryStatus secondaryStatus = CellSecondaryStatus.Blank,
+                                                      int bombsNearby = 0) => new(isBomb, secondaryStatus, bombsNearby);
+    }
+}
